752ba4514ceb0f1a304de3d8cced7ad0eaf0a824
Joshua <519877651@qq.com>
2022-07-07 11:25:27 +0800

    Add files via upload

---
+++ SEM.r
@@ -0,0 +1,89 @@
+#SEM
+
+library(piecewiseSEM)
+
+
+data=metanew
+data[,5:44]=scale(data[,5:44],center=T,scale=T)
+data=as.data.frame(data)
+
+#SEM model for cycfac motif 
+sem.model1 <- psem (
+lm(biodiversity ~ fragility+ cycfac_total + modularity +Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C,  data = data),
+lm(fragility~ cycfac_total + modularity +Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C, data = data),
+lm(modularity~  cycfac_total +Distance_from_equator+Elevation+TSEA +MAP  +pH+Texture+Soil_C ,data = data),
+lm(cycfac_total~  Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C, data = data),
+lm(pH~ Distance_from_equator+Elevation+TSEA+MAP , data = data),
+lm(Soil_C~ Distance_from_equator+Elevation+TSEA+MAP+pH +Texture  , data = data),
+lm(Texture~ Distance_from_equator+Elevation+TSEA+MAP , data = data),
+lm(TSEA~ Distance_from_equator+Elevation, data = data),
+lm(MAP~Distance_from_equator+Elevation +TSEA  , data = data)
+)
+#export result
+summary(sem.model1)
+
+
+
+#SEM model for facmcom motif
+sem.model2 <- psem (
+lm(biodiversity ~ fragility+ facmcom_total + modularity +Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C,  data = data),
+lm(fragility~ facmcom_total + modularity +Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C, data = data),
+lm(modularity~  facmcom_total +Distance_from_equator+Elevation+TSEA +MAP  +pH+Texture+Soil_C ,data = data),
+lm(facmcom_total~  Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C, data = data),
+lm(pH~ Distance_from_equator+Elevation+TSEA+MAP , data = data),
+lm(Soil_C~ Distance_from_equator+Elevation+TSEA+MAP+pH +Texture  , data = data),
+lm(Texture~ Distance_from_equator+Elevation+TSEA+MAP , data = data),
+lm(TSEA~ Distance_from_equator+Elevation, data = data),
+lm(MAP~Distance_from_equator+Elevation +TSEA  , data = data)
+)
+#export result
+summary(sem.model2)
+
+
+#SEM model for trancom motif
+sem.model3 <- psem (
+lm(biodiversity ~ fragility+ trancom_total + modularity +Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C,  data = data),
+lm(fragility~ trancom_total + modularity +Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C, data = data),
+lm(modularity~  trancom_total +Distance_from_equator+Elevation+TSEA +MAP  +pH+Texture+Soil_C ,data = data),
+lm(trancom_total~  Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C, data = data),
+lm(pH~ Distance_from_equator+Elevation+TSEA+MAP , data = data),
+lm(Soil_C~ Distance_from_equator+Elevation+TSEA+MAP+pH +Texture  , data = data),
+lm(Texture~ Distance_from_equator+Elevation+TSEA+MAP , data = data),
+lm(TSEA~ Distance_from_equator+Elevation, data = data),
+lm(MAP~Distance_from_equator+Elevation +TSEA  , data = data)
+)
+#export result
+summary(sem.model3)
+
+
+#SEM model for facmcom motif within eukaryotes
+sem.model4 <- psem (
+lm(biodiversity ~ fragility+ facmcom_euk + modularity +Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C,  data = data),
+lm(fragility~ facmcom_euk + modularity +Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C, data = data),
+lm(modularity~  facmcom_euk +Distance_from_equator+Elevation+TSEA +MAP  +pH+Texture+Soil_C ,data = data),
+lm(facmcom_euk~  Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C, data = data),
+lm(pH~ Distance_from_equator+Elevation+TSEA+MAP , data = data),
+lm(Soil_C~ Distance_from_equator+Elevation+TSEA+MAP+pH +Texture  , data = data),
+lm(Texture~ Distance_from_equator+Elevation+TSEA+MAP , data = data),
+lm(TSEA~ Distance_from_equator+Elevation, data = data),
+lm(MAP~Distance_from_equator+Elevation +TSEA  , data = data)
+)
+#export result
+summary(sem.model4)
+
+
+
+#SEM model for cycfac motif across domains
+sem.model5 <- psem (
+lm(biodiversity ~ fragility+ cycfac_across + modularity +Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C,  data = data),
+lm(fragility~ cycfac_across + modularity +Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C, data = data),
+lm(modularity~  cycfac_across +Distance_from_equator+Elevation+TSEA +MAP  +pH+Texture+Soil_C ,data = data),
+lm(cycfac_across~  Distance_from_equator+Elevation+TSEA+MAP+pH+Texture+Soil_C, data = data),
+lm(pH~ Distance_from_equator+Elevation+TSEA+MAP , data = data),
+lm(Soil_C~ Distance_from_equator+Elevation+TSEA+MAP+pH +Texture  , data = data),
+lm(Texture~ Distance_from_equator+Elevation+TSEA+MAP , data = data),
+lm(TSEA~ Distance_from_equator+Elevation, data = data),
+lm(MAP~Distance_from_equator+Elevation +TSEA  , data = data)
+)
+#export result
+summary(sem.model5)
---
+++ motif exploration.r
@@ -0,0 +1,351 @@
+#explore seven network motifs
+
+library(reshape)
+library(vegan)
+library(igraph)
+library(bipartite)
+library(parallel)
+library(foreach)
+library(doParallel)
+library(arm)
+library(rstanarm)
+library(car)
+library(lme4)
+library(lmerTest)
+library(effects)
+library(emmeans)
+library(truncnorm)
+library(lattice)
+library(gplots)
+library(ggplot2)
+library(deSolve)
+library(mgcv)
+library(itsadug)
+library(tidyr)
+library(ggridges)
+library(tidyverse)
+library(pscl)
+library(betalink)
+library(glmmTMB)
+library(corrgram)
+library(bayesplot)
+
+#mat is the adjacency matrix obained from the network inference
+
+#cycle facilitation (cycfac)
+cycfac<-function(mat){
+
+diag(mat)=0  
+mat[!lower.tri(mat, diag = TRUE)] <- 0
+
+matp<- mat; matp[which(matp<0)]<-0; matp[which(matp>0)]<-1
+nsp<-nrow(matp)
+ntrip<-0
+
+for(i in 1:nsp){
+# first, subset two species facilitated by a third
+nei<-sum(matp[,i])
+idnei<-as.numeric(which(matp[,i]==1))
+
+if(nei>=2){
+# number of search = number of possible pairs = n!/2!(n-2)!
+nos<- factorial(nei)/(2*factorial(nei-2))
+counter<-0
+
+# then, for each facilitated pair,
+# look if they are both positively associated
+for(k in 1:(nei-1)){for(z in (k+1):nei){
+counter<-counter+1
+if(matp[idnei[k],idnei[z]]==1|matp[idnei[z],idnei[k]]==1)
+ntrip <- ntrip+1
+}
+#if(counter==nos) break
+}
+}
+}
+return(ntrip)
+}
+
+
+# cycle competition (cyccom)
+cyccom<-function(mat){
+
+diag(mat)=0  
+mat[!lower.tri(mat, diag = TRUE)] <- 0
+
+matn<- mat; matn[which(matn>0)]<-0; matn[which(matn<0)]<-1
+nsp<-nrow(matn)
+ntrip<-0
+
+for(i in 1:nsp){
+# first, subset two species competition by a third
+nei<-sum(matn[,i])
+idnei<-as.numeric(which(matn[,i]==1))
+
+if(nei>=2){
+# number of search = number of possible pairs = n!/2!(n-2)!
+nos<- factorial(nei)/(2*factorial(nei-2))
+counter<-0
+
+# then, for each competition pair,
+# look if they are both negatively associated
+for(k in 1:(nei-1)){for(z in (k+1):nei){
+counter<-counter+1
+if(matn[idnei[k],idnei[z]]==1|matn[idnei[z],idnei[k]]==1)
+ntrip <- ntrip+1
+}
+#if(counter==nos) break
+}
+}
+}
+return(ntrip)
+}
+
+
+
+# facilitation-mediated competition (facmcom)
+facmcom<-function(mat){
+
+diag(mat)=0  
+mat[!lower.tri(mat, diag = TRUE)] <- 0
+
+nsp<-nrow(mat)
+mat[which(is.na(mat))]<-0
+matp<- mat; matp[which(matp<0)]<-0; matp[which(matp>0)]<-1
+matn<- mat; matn[which(matn>0)]<-0; matn[which(matn<0)]<-1
+#
+ntrip<-0
+for(i in 1:nsp){
+# first, subset two species facilitated by a third
+nei<-sum(matn[,i])
+idnei<-as.numeric(which(matn[,i]==1))
+
+if(nei>=2){
+# number of search = number of possible pairs = n!/2!(n-2)!
+nos<- factorial(nei)/(2*factorial(nei-2))
+counter<-0
+
+# then, for each facilitated pair,
+# look if they are both negatively associated
+for(k in 1:(nei-1)){for(z in (k+1):nei){
+counter<-counter+1
+if(matp[idnei[k],idnei[z]]==1|matp[idnei[z],idnei[k]]==1)
+ntrip <- ntrip+1
+}
+#if(counter==nos) break
+}
+}
+}
+return(ntrip)
+}
+
+
+# competition-mediated facilitation  (commfac)
+commfac<-function(mat){
+
+diag(mat)=0  
+mat[!lower.tri(mat, diag = TRUE)] <- 0
+
+nsp<-nrow(mat)
+mat[which(is.na(mat))]<-0
+matp<- mat; matp[which(matp<0)]<-0; matp[which(matp>0)]<-1
+matn<- mat; matn[which(matn>0)]<-0; matn[which(matn<0)]<-1
+ntrip<-0
+
+for(i in 1:nsp){
+# first, subset two species competing with a third
+nei<-sum(matp[i,])
+idnei<-as.numeric(which(matp[i,]==1))
+
+if(nei>=2){
+# number of search = number of possible pairs = n!/2!(n-2)!
+nos<- factorial(nei)/(2*factorial(nei-2))
+counter<-0
+
+# then, for each competing pair,
+# look if they are both positively associated
+for(k in 1:(nei-1)){for(z in (k+1):nei){
+counter<-counter+1
+if(matn[idnei[k],idnei[z]]==1|matn[idnei[z],idnei[k]]==1)
+ntrip <- ntrip+1
+}
+#if(counter==nos) break
+}
+}
+}
+return(ntrip)
+}
+
+
+# transitive facilitation  (tranfac)
+tranfac<-function(mat){
+
+diag(mat)=0  
+mat[!lower.tri(mat, diag = TRUE)] <- 0
+
+nsp<-nrow(mat)
+mat[which(is.na(mat))]<-0
+matp<- mat;  matp[which(matp>0)]<-1;matp[which(matp<0)]<-0
+
+ntrip<-0
+for(i in 1:nsp){
+# first, subset two species facilitated by a third
+nei<-sum(matp[,i])
+idnei<-as.numeric(which(matp[,i]==1))
+
+if(nei>=2){
+# number of search = number of possible pairs = n!/2!(n-2)!
+nos<- factorial(nei)/(2*factorial(nei-2))
+counter<-0
+
+# then, for each facilitated pair,
+# look if they are both negatively associated
+for(k in 1:(nei-1)){for(z in (k+1):nei){
+counter<-counter+1
+if(mat[idnei[z],idnei[k]]==0&mat[idnei[k],idnei[z]]==0)
+ntrip <- ntrip+1
+}
+#if(counter==nos) break
+}
+}
+}
+return(ntrip)
+}
+
+
+# transitive competition  (trancom)
+trancom<-function(mat){
+
+diag(mat)=0  
+mat[!lower.tri(mat, diag = TRUE)] <- 0
+
+nsp<-nrow(mat)
+mat[which(is.na(mat))]<-0
+matn<- mat;  matn[which(matn>0)]<-0;matn[which(matn<0)]<-1
+ntrip<-0
+
+for(i in 1:nsp){
+# first, subset two species facilitated by a third
+nei<-sum(matn[,i])
+idnei<-as.numeric(which(matn[,i]==1))
+
+if(nei>=2){
+# number of search = number of possible pairs = n!/2!(n-2)!
+nos<- factorial(nei)/(2*factorial(nei-2))
+counter<-0
+
+# then, for each facilitated pair,
+# look if they are both negatively associated
+for(k in 1:(nei-1)){for(z in (k+1):nei){
+counter<-counter+1
+if(mat[idnei[z],idnei[k]]==0&mat[idnei[k],idnei[z]]==0)
+ntrip <- ntrip+1
+}
+#if(counter==nos) break
+}
+}
+}
+return(ntrip)
+}
+
+
+# transitive competition and facilitation (trancomfac)
+trancomfac0<-function(mat){
+
+diag(mat)=0  
+mat[!lower.tri(mat, diag = TRUE)] <- 0
+nsp<-nrow(mat)
+
+#Set all positive and negative associations to 1
+mat[which(is.na(mat))]<-0
+mat[which(mat<0)]=1; mat[which(mat>0)]=1
+ntrip<-0
+
+for(i in 1:nsp){
+# first, subset two species cooccuring with a third
+nei<-sum(mat[,i])
+idnei<-as.numeric(which(mat[,i]==1))
+
+if(nei>=2){
+# number of search = number of possible pairs = n!/2!(n-2)!
+nos<- factorial(nei)/(2*factorial(nei-2))
+counter<-0
+
+# then, for each facilitated pair, (actually, including --, ++, +- three types)
+# look if they are both neutural associated, namely no correlation with the third.
+for(k in 1:(nei-1)){for(z in (k+1):nei){
+counter<-counter+1
+if(mat[idnei[z],idnei[k]]==0&mat[idnei[k],idnei[z]]==0)
+ntrip <- ntrip+1
+}
+#if(counter==nos) break
+}
+}
+}
+return(ntrip)
+}
+
+#Trancomfac is obtained by subtracting trancom and tranfac from all transive association 
+trancomfac<-function(mat){
+nsp<-nrow(mat)
+mat[which(is.na(mat))]<-0
+triads<-  trancomfac0(mat) -trancom(mat)-tranfac(mat)
+return(triads)
+}
+
+
+#summary all types of motifs
+netmotif=function(mat){
+data <- c()
+data = c(data, cycfac(mat))
+data = c(data, cyccom(mat))
+data = c(data, facmcom(mat))
+data = c(data, commfac(mat))
+data = c(data, tranfac(mat))
+data = c(data, trancom(mat))
+data = c(data, trancomfac(mat))
+data=t(as.data.frame(data))
+colnames(data)=c("cycfac","cyccom","facmcom","commfac","tranfac","trancom","trancomfac")
+return(data)
+}
+
+
+
+
+#########################################################################################
+#motifs within real networks and random networks
+#########################################################################################
+#motifs within real networks
+#take the clustering region #1 as an example
+#get the number of motifs in the ecological network
+motif=c()
+motif=as.data.frame(netmotif(as.matrix(adj_mat1))) 
+
+#import edgelist file to R space
+edge1 <- read.csv("edge1.csv")
+
+#motifs within random networks
+#create random networks (rn)
+rn1=list()
+for (x in seq_len(999L)) {
+df=matrix(data = NA,nrow = nrow(edge1),ncol = 3,byrow = FALSE,dimnames = NULL)
+df[,1] = edge1[sample(nrow(edge1), nrow(edge1)),1 ]
+df[,2] = edge1[sample(nrow(edge1), nrow(edge1)),2 ]
+df[,3] = edge1[sample(nrow(edge1), nrow(edge1)),4 ]
+rn1[[x]]=df
+}
+
+random_motif1=c()
+for (x in seq_len(999L)) {
+ignew=graph_from_edgelist(as.matrix(rn1[[x]][,-3]) , directed = FALSE) #elist is 0.45edge
+ignew=set_edge_attr(ignew, 'weight', index = E(ignew),  as.numeric(rn1[[x]][,3]) )
+a=as.matrix(as_adjacency_matrix( ignew,type = "both", names = TRUE,attr="weight"))  
+re=as.data.frame(netmotif(a)) 
+random_motif1=rbind(random_motif1,re)
+print(x)
+}
+
+write.csv(random_motif1,"random_motif1.csv")
+
+
+#overrepresentation of network motifs could be calculated as described by M&M using EXCEL
