69a7d2e88bbe8470a872cdd9c4fe5db8b545c96f
Joshua <519877651@qq.com>
2025-01-07 15:32:15 +0800

    Update motif exploration.r

--- motif exploration.r
+++ motif exploration.r
@@ -184,31 +184,49 @@ return(ntrip)
 ############################################################################
 # transitive facilitation  (tranfac)
 ############################################################################
+
 tranfac<-function(mat){
 
-diag(mat)=0  
-mat[!lower.tri(mat, diag = TRUE)] <- 0
+# 计算每行和每列的 1 的数量
+row_sums <- rowSums(abs(mat))
+col_sums <- colSums(abs(mat))
 
-nsp<-nrow(mat)
-mat[which(is.na(mat))]<-0
-matp<- mat;  matp[which(matp>0)]<-1;matp[which(matp<0)]<-0
+# 按照 1 的数量对行和列进行排序（降序）
+sorted_rows <- order(row_sums, decreasing = TRUE)
+sorted_cols <- order(col_sums, decreasing = TRUE)
 
+# 重新排序矩阵
+sorted_mat <- mat[sorted_rows, sorted_cols]
+
+
+
+diag(sorted_mat)=0  
+sorted_mat[!lower.tri(sorted_mat, diag = TRUE)] <- 0
+
+nsp<-nrow(sorted_mat)
+sorted_mat[which(is.na(sorted_mat))]<-0
+matp<- sorted_mat; matp[which(matp<0)]<-0; matp[which(matp>0)]<-1
+matn<- sorted_mat; matn[which(matn>0)]<-0; matn[which(matn<0)]<-1
+
+matt<- sorted_mat; matt[which(matt>0)]<-1; matt[which(matt<0)]<-1
+#
 ntrip<-0
 for(i in 1:nsp){
-# first, subset two species facilitated by a third
-nei<-sum(matp[,i])
-idnei<-as.numeric(which(matp[,i]==1))
+# first, subset two species associated by a third
+nei<-sum(matt[,i])
+idnei<-as.numeric(which(matt[,i]==1))
 
 if(nei>=2){
 # number of search = number of possible pairs = n!/2!(n-2)!
 nos<- factorial(nei)/(2*factorial(nei-2))
 counter<-0
 
-# then, for each facilitated pair,
-# look if they are both negatively associated
+# then, for each competed pair,
+# look if they are both  associated
 for(k in 1:(nei-1)){for(z in (k+1):nei){
 counter<-counter+1
-if(mat[idnei[z],idnei[k]]==0&mat[idnei[k],idnei[z]]==0)
+if ((matn[idnei[z], idnei[k]] + matn[ idnei[z],i] + matn[ idnei[k],i] == 0) & 
+    (matp[idnei[z], idnei[k]] + matp[ idnei[z],i] + matp[ idnei[k],i] == 2))
 ntrip <- ntrip+1
 }
 #if(counter==nos) break
@@ -224,29 +242,46 @@ return(ntrip)
 ############################################################################
 trancom<-function(mat){
 
-diag(mat)=0  
-mat[!lower.tri(mat, diag = TRUE)] <- 0
+# 计算每行和每列的 1 的数量
+row_sums <- rowSums(abs(mat))
+col_sums <- colSums(abs(mat))
 
-nsp<-nrow(mat)
-mat[which(is.na(mat))]<-0
-matn<- mat;  matn[which(matn>0)]<-0;matn[which(matn<0)]<-1
+# 按照 1 的数量对行和列进行排序（降序）
+sorted_rows <- order(row_sums, decreasing = TRUE)
+sorted_cols <- order(col_sums, decreasing = TRUE)
+
+# 重新排序矩阵
+sorted_mat <- mat[sorted_rows, sorted_cols]
+
+
+
+diag(sorted_mat)=0  
+sorted_mat[!lower.tri(sorted_mat, diag = TRUE)] <- 0
+
+nsp<-nrow(sorted_mat)
+sorted_mat[which(is.na(sorted_mat))]<-0
+matp<- sorted_mat; matp[which(matp<0)]<-0; matp[which(matp>0)]<-1
+matn<- sorted_mat; matn[which(matn>0)]<-0; matn[which(matn<0)]<-1
+
+matt<- sorted_mat; matt[which(matt>0)]<-1; matt[which(matt<0)]<-1
+#
 ntrip<-0
-
 for(i in 1:nsp){
-# first, subset two species facilitated by a third
-nei<-sum(matn[,i])
-idnei<-as.numeric(which(matn[,i]==1))
+# first, subset two species associated by a third
+nei<-sum(matt[,i])
+idnei<-as.numeric(which(matt[,i]==1))
 
 if(nei>=2){
 # number of search = number of possible pairs = n!/2!(n-2)!
 nos<- factorial(nei)/(2*factorial(nei-2))
 counter<-0
 
-# then, for each facilitated pair,
-# look if they are both negatively associated
+# then, for each competed pair,
+# look if they are both  associated
 for(k in 1:(nei-1)){for(z in (k+1):nei){
 counter<-counter+1
-if(mat[idnei[z],idnei[k]]==0&mat[idnei[k],idnei[z]]==0)
+if ((matn[idnei[z], idnei[k]] + matn[ idnei[z],i] + matn[ idnei[k],i] == 2) & 
+    (matp[idnei[z], idnei[k]] + matp[ idnei[z],i] + matp[ idnei[k],i] == 0))
 ntrip <- ntrip+1
 }
 #if(counter==nos) break
@@ -260,32 +295,48 @@ return(ntrip)
 ############################################################################
 # transitive competition and facilitation (trancomfac)
 ############################################################################
-trancomfac0<-function(mat){
+trancomfac<-function(mat){
 
-diag(mat)=0  
-mat[!lower.tri(mat, diag = TRUE)] <- 0
-nsp<-nrow(mat)
+# 计算每行和每列的 1 的数量
+row_sums <- rowSums(abs(mat))
+col_sums <- colSums(abs(mat))
 
-#Set all positive and negative associations to 1
-mat[which(is.na(mat))]<-0
-mat[which(mat<0)]=1; mat[which(mat>0)]=1
+# 按照 1 的数量对行和列进行排序（降序）
+sorted_rows <- order(row_sums, decreasing = TRUE)
+sorted_cols <- order(col_sums, decreasing = TRUE)
+
+# 重新排序矩阵
+sorted_mat <- mat[sorted_rows, sorted_cols]
+
+
+
+diag(sorted_mat)=0  
+sorted_mat[!lower.tri(sorted_mat, diag = TRUE)] <- 0
+
+nsp<-nrow(sorted_mat)
+sorted_mat[which(is.na(sorted_mat))]<-0
+matp<- sorted_mat; matp[which(matp<0)]<-0; matp[which(matp>0)]<-1
+matn<- sorted_mat; matn[which(matn>0)]<-0; matn[which(matn<0)]<-1
+
+matt<- sorted_mat; matt[which(matt>0)]<-1; matt[which(matt<0)]<-1
+#
 ntrip<-0
-
 for(i in 1:nsp){
-# first, subset two species cooccuring with a third
-nei<-sum(mat[,i])
-idnei<-as.numeric(which(mat[,i]==1))
+# first, subset two species associated by a third
+nei<-sum(matt[,i])
+idnei<-as.numeric(which(matt[,i]==1))
 
 if(nei>=2){
 # number of search = number of possible pairs = n!/2!(n-2)!
 nos<- factorial(nei)/(2*factorial(nei-2))
 counter<-0
 
-# then, for each facilitated pair, (actually, including --, ++, +- three types)
-# look if they are both neutural associated, namely no correlation with the third.
+# then, for each competed pair,
+# look if they are both  associated
 for(k in 1:(nei-1)){for(z in (k+1):nei){
 counter<-counter+1
-if(mat[idnei[z],idnei[k]]==0&mat[idnei[k],idnei[z]]==0)
+if ((matn[idnei[z], idnei[k]] + matn[ idnei[z],i] + matn[ idnei[k],i] == 1) & 
+    (matp[idnei[z], idnei[k]] + matp[ idnei[z],i] + matp[ idnei[k],i] == 1))
 ntrip <- ntrip+1
 }
 #if(counter==nos) break
@@ -293,14 +344,6 @@ ntrip <- ntrip+1
 }
 }
 return(ntrip)
-}
-
-#Trancomfac is obtained by subtracting trancom and tranfac from all transive association 
-trancomfac<-function(mat){
-nsp<-nrow(mat)
-mat[which(is.na(mat))]<-0
-triads<-  trancomfac0(mat) -trancom(mat)-tranfac(mat)
-return(triads)
 }
 
 
